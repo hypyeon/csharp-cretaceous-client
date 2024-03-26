58960aea8c8509ca46d9dc9e34135d675581b03a
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-25 17:16:18 -0700

    add full CRUD

---
+++ CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CretaceousClient.Models;
+
+public class ErrorViewModel
+{
+    #nullable enable
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
